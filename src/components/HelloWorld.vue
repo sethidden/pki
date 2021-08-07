d1f61d71893284f9a6ba0b2bd10f84e71561a676
Artur Tagisow <artur@tagisow.dev>
2021-08-07 11:26:06 +0200

    add icons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <template>
 <div class="flex">
-  <span ref="publicKeyElement" :style="publicDragPosition">Public key</span>
-  <span ref="privateKeyElement" :style="privateDragPosition">Private key</span>
+  <span ref="publicKeyElement" :style="publicDragPosition">
+    <IconKey />
+    <IconGlobe />
+    Public key
+  </span>
+  <span ref="privateKeyElement" :style="privateDragPosition">
+    <IconKey />
+    <IconUserSecret />
+    Private key
+  </span>
 </div>
 <div ref="textElement">{{ textContent }}</div>
 </template>
 
 <script setup lang="ts">
+import IconKey from 'virtual:vite-icons/fa-solid/key'
+import IconGlobe from 'virtual:vite-icons/fa-solid/globe'
+import IconUserSecret from 'virtual:vite-icons/fa-solid/user-secret'
 import {useMouseInElement, useMousePressed} from '@vueuse/core'
 import {useDragAndDrop} from '../logics/useDragAndDrop'
 import { computed, ref, watch } from 'vue'
