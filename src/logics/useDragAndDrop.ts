2990a0bcc2858fc01839e1bb8b76e7af1b0bf2df
Artur Tagisow <artur@tagisow.dev>
2021-08-31 12:05:06 +0200

    Remove argument optionality because _.curry gets lost with typing

--- src/logics/useDragAndDrop.ts
+++ src/logics/useDragAndDrop.ts
@@ -1,7 +1,7 @@
 import { useMouseInElement, useMousePressed } from "@vueuse/core";
 import { computed, Ref, watch } from "vue";
 
-export const useDragAndDrop = (dragged: Ref<unknown>, target: Ref<unknown>, onDroppedInside?: () => void) => {
+export const useDragAndDrop = (target: Ref<unknown>, dragged: Ref<unknown>,  onDroppedInside: () => void) => {
   const {x, y, isOutside} = useMouseInElement(target);
   const { pressed } = useMousePressed({ target: dragged })
   const dragPosition = computed(() => pressed.value ? { left: `${x.value}px`, top: `${y.value}px`, position: 'absolute' } : null)
